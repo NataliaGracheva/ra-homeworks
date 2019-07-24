d894e8e623f572d553d3cd43c9a1719bfe78d7d2
NataliaGracheva <40542294+NataliaGracheva@users.noreply.github.com>
2019-07-24 12:33:58 +0500

    Create ItemModel.js

---
+++ components/store-func/src/models/ItemModel.js
@@ -0,0 +1,12 @@
+export class ItemModel {
+    constructor(entity) {
+        Object.assign(this, {
+            brand: '',
+            title: '',
+            description: '',
+            descriptionFull: '',
+            price: 0,
+            currency: ''
+        }, entity);
+    }
+}
