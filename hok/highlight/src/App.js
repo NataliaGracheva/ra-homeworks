67365b5733c10b8d16b6f478e0bf5c4e41115317
NataliaGracheva <40542294+NataliaGracheva@users.noreply.github.com>
2019-08-29 16:34:14 +0500

    done

---
+++ hok/highlight/src/App.js
@@ -0,0 +1,111 @@
+import React, { useState } from 'react';
+
+function Wrap(func) {
+  return function(props) {
+    if (props.views > 1000) {
+      return Popular({ ...props, children: func(props) });
+    } 
+    if (props.views < 100) {
+      return New({ ...props, children: func(props) });
+    }
+    
+    return func(props);
+    
+  };
+}
+
+const WrapVideo = Wrap(Video);
+const WrapArticle = Wrap(Article);
+
+function New(props) {
+    return (
+        <div className="wrap-item wrap-item-new">
+            <span className="label">New!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Popular(props) {
+    return (
+        <div className="wrap-item wrap-item-popular">
+            <span className="label">Popular!</span>
+            {props.children}
+        </div>
+    )
+};
+
+function Article(props) {
+    return (
+        <div className="item item-article">
+            <h3><a href="#" title={props.title}>{props.title}</a></h3>
+            <p className="views">Прочтений: {props.views}</p>
+        </div>
+    )
+};
+
+function Video(props) {
+    return (
+        <div className="item item-video">
+            <iframe src={props.url} frameBorder="0" allow="autoplay; encrypted-media" allowFullScreen title={props.url}></iframe>
+            <p className="views">Просмотров: {props.views}</p>
+        </div>
+    )
+};
+
+function List(props) {
+    return props.list.map(item => {
+        switch (item.type) {
+            case 'video':
+                return (
+                    // <Video {...item} />
+                    <WrapVideo key={item.url} {...item} />
+                );
+
+            case 'article':
+                return (
+                    // <Article {...item} />
+                    <WrapArticle key={item.title} {...item} />
+                );
+        }
+    });
+};
+
+export default function App() {
+    const [list, setList] = useState([
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/rN6nlNC9WQA?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 50
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/dVkK36KOcqs?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 12
+        },
+        {
+            type: 'article',
+            title: 'Невероятные события в неизвестном поселке...',
+            views: 175
+        },
+        {
+            type: 'article',
+            title: 'Секретные данные были раскрыты!',
+            views: 1532
+        },
+        {
+            type: 'video',
+            url: 'https://www.youtube.com/embed/TKmGU77INaM?rel=0&amp;controls=0&amp;showinfo=0',
+            views: 4253
+        },
+        {
+            type: 'article',
+            title: 'Кот Бегемот обладает невероятной...',
+            views: 12,
+        },
+    ]);
+
+    return (
+        <List list={list} />
+    );
+}
